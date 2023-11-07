e33093bab67089f7ad4502b2bcda894504ee91ad
mosfeqanik <mosfeqanik01@gmail.com>
2023-11-07 16:15:23 +0600

    widgets test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:al_hadith_books/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const AlHadithApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
